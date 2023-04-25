3f7bb1af4e6263a0addea1010ef0146cfdc385d5
Summer <xiaoxipu@umich.edu>
2023-04-25 12:08:28 -0700

    all files

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import MainPage from './mainPage';
+import Login from './login';
+
+function App() {
+  // A state that represents if user is logged in
+  // If user is logged in:
+    // Return mainpage component
+  return (
+    
+    <div>
+      <MainPage />
+    </div>
+
+  )
+    
+  // Else 
+    // Return login component
+    
+}
+
+export default App;
+
